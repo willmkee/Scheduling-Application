ba18b08a00c3f2f8fc2eb629cd1a926483b44ca2
Will <keewill57@gmail.com>
2023-01-03 14:20:42 -0700

    Created loginform UI //Need to add FXIDs

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -12,8 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../View/loginForm.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Log-In");
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
 
     }
