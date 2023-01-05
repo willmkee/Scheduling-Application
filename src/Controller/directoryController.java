90a43b29309a7d6180921cd85a744bc6f8e252d1
Will <keewill57@gmail.com>
2023-01-04 17:22:01 -0700

    Designed directory.fxml.fxml Added fields and methods to directoryController.java

--- src/Controller/directoryController.java
+++ src/Controller/directoryController.java
@@ -1,4 +1,31 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class directoryController {
+    public Button viewUpdateCustomerButton;
+    public Button addCustomerButton;
+    public Button viewUpdateAppointmentsButton;
+    public Button addAppointmentButton;
+    public Button viewReportsButton;
+    public Button exitButton;
+
+    public void onViewUpdate(ActionEvent actionEvent) {
+    }
+
+    public void onAddCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onViewUpdateAppointments(ActionEvent actionEvent) {
+    }
+
+    public void onAddAppointment(ActionEvent actionEvent) {
+    }
+
+    public void onViewReports(ActionEvent actionEvent) {
+    }
+
+    public void onExit(ActionEvent actionEvent) {
+    }
 }
