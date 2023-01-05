2769e4d58397604d59cd6285b19c01812b4d834e
Will <keewill57@gmail.com>
2023-01-04 17:39:12 -0700

    Designed addCustomer.fxml Added fields and methods to addCustomerController.java

--- src/Controller/addCustomerController.java
+++ src/Controller/addCustomerController.java
@@ -1,4 +1,33 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class addCustomerController {
+    public Label addCustomerLabel;
+    public Label addCustomerIdLabel;
+    public TextField addCustomerIdTextField;
+    public Label addCustomerNameLabel;
+    public TextField addCustomerNameTextField;
+    public Label addAddressLabel;
+    public TextField addAddressTextField;
+    public Label addPostalCodeLabel;
+    public TextField addPostalCodeTextField;
+    public Label addPhoneNumberLabel;
+    public TextField addPhoneNumberTextField;
+    public Label addCountryLabel;
+    public ComboBox addCountryComboBox;
+    public Label addStateProvinceLabel;
+    public ComboBox addStateProvinceComboBox;
+    public Button addSaveChangesButton;
+    public Button cancelButton;
+
+    public void onAddSaveChanges(ActionEvent actionEvent) {
+    }
+
+    public void onCancel(ActionEvent actionEvent) {
+    }
 }
