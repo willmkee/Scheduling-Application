750bdc36704cdb707dffd78fe6ed54c43eed56c0
Will <keewill57@gmail.com>
2023-01-09 14:26:20 -0700

    completed appointments.fxml Added fields and methods to appointmentsController.java

--- src/Controller/appointmentsController.java
+++ src/Controller/appointmentsController.java
@@ -1,4 +1,68 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class appointmentsController {
+    public Label appointmentsLabel;
+    public TableView appointmentsTableView;
+    public TableColumn appointmentTableCol;
+    public TableColumn titleTableCol;
+    public TableColumn descriptionTableCol;
+    public TableColumn locationTableCol;
+    public TableColumn contactTableCol;
+    public TableColumn typeTableCol;
+    public TableColumn startDatetimeTableCol;
+    public TableColumn endDatetimeTableCol;
+    public TableColumn customerIDTableCol;
+    public TableColumn userIdTableCol;
+    public RadioButton allAppointmentsRadioButton;
+    public ToggleGroup appointmentRadioButtons;
+    public RadioButton appointmentsByWeekRadioButton;
+    public RadioButton appointmentsByMonthRadioButton;
+    public Label appointmentIdLabel;
+    public TextField appointmentIdTextField;
+    public Label appointmentTitleLabel;
+    public TextField appointmentTitleTextField;
+    public Label appointmentDescriptionLabel;
+    public TextField appointmentDescriptionTextField;
+    public Label appointmentLocationLabel;
+    public TextField appointmentLocationTextField;
+    public Label contactLabel;
+    public ComboBox contactComboBox;
+    public Label typeLabel;
+    public TextField typeTextField;
+    public Label startDateLabel;
+    public DatePicker startDateDatePicker;
+    public Label startTimeLabel;
+    public ComboBox startTimeComboBox;
+    public Label endDateLabel;
+    public DatePicker endDateDatePicker;
+    public Label endTimeLabel;
+    public ComboBox endTimeComboBox;
+    public Button updateAppointmentButton;
+    public Button deleteAppointmentButton;
+    public Button addAppointmentButton;
+    public Button mainMenuButton;
+
+    public void onAllAppointments(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentsByWeek(ActionEvent actionEvent) {
+    }
+
+    public void onAppointmentsByMonth(ActionEvent actionEvent) {
+    }
+
+    public void onUpdateAppointment(ActionEvent actionEvent) {
+    }
+
+    public void onDeleteAppointment(ActionEvent actionEvent) {
+    }
+
+    public void onAddAppointment(ActionEvent actionEvent) {
+    }
+
+    public void onMainMenu(ActionEvent actionEvent) {
+    }
 }
