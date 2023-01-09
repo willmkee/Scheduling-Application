c6707f78f43fbfab701c747db766b304abba1007
Will <keewill57@gmail.com>
2023-01-09 14:55:12 -0700

    completed addAppointment.fxml Added fields and methods to addAppointmentController.java

--- src/Controller/addAppointmentController.java
+++ src/Controller/addAppointmentController.java
@@ -1,4 +1,40 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class addAppointmentController {
+    public Label addAppointmentLabel;
+    public Label addAppointmentIdLabel;
+    public TextField addAppointmentIdTextField;
+    public Label addAppointmentTitleLabel;
+    public TextField addAppointmentTitleTextField;
+    public Label addAppointmentDescriptionLabel;
+    public TextField addAppointmentDescriptionTextField;
+    public Label addAppointmentLocationLabel;
+    public TextField addAppointmentLocationTextField;
+    public Label addContactLabel;
+    public ComboBox addContactComboBox;
+    public Label addAppointmentCustomerIdLabel;
+    public TextField addAppointmentCustomerIdTextField;
+    public Label addAppointmentTypeLabel;
+    public TextField addAppointmentTypeTextField;
+    public Label addAppointmentStartDateLabel;
+    public DatePicker addAppointmentStartDateDatePicker;
+    public Label addAppointmentEndDateLabel;
+    public DatePicker addAppointmentEndDateDatePicker;
+    public Label addAppointmentEndTimeLabel;
+    public ComboBox addAppointmentEndTimeComboBox;
+    public Label addAppointmentUserIdLabel;
+    public TextField addAppointmentUserIdTextField;
+    public Button saveAppointmentButton;
+    public Button cancelButton;
+    public Label addAppointmentStartTimeLabel;
+    public ComboBox addAppointmentStartTimeComboBox;
+
+    public void onSaveAppointment(ActionEvent actionEvent) {
+    }
+
+    public void onCancel(ActionEvent actionEvent) {
+    }
 }
