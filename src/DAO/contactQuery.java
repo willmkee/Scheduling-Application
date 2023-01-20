5c95181749c1e9469cf95e00cf42e4bb3c4f2649
Will <keewill57@gmail.com>
2023-01-20 15:05:18 -0700

    messing with comboboxes and failing

--- src/DAO/contactQuery.java
+++ src/DAO/contactQuery.java
@@ -4,7 +4,7 @@ import Model.Contact;
 import javafx.collections.ObservableList;
 
 public class contactQuery {
-    public static ObservableList<Contact> getAllContacts() {
+    /*public static ObservableList<Contact> getAllContacts() {
 
-    }
+    }*/
 }
