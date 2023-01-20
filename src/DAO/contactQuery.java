cc862e3762588b88ee3e00b6e72fb7be7a6183ba
Will <keewill57@gmail.com>
2023-01-20 13:08:03 -0700

    messing with comboboxes and failing

---
+++ src/DAO/contactQuery.java
@@ -0,0 +1,10 @@
+package DAO;
+
+import Model.Contact;
+import javafx.collections.ObservableList;
+
+public class contactQuery {
+    public static ObservableList<Contact> getAllContacts() {
+
+    }
+}
