66326d67292598b9dd33efffc94c1827fcb8732a
LabUser <wkee@wgu.edu>
2023-02-07 12:42:41 -0700

    updateAppointment() and onSaveChanges do not work

--- src/helper/JDBC.java
+++ src/helper/JDBC.java
@@ -9,7 +9,7 @@ public abstract class JDBC {
     private static final String vendor = ":mysql:";
     private static final String location = "//localhost/";
     private static final String databaseName = "client_schedule";
-    private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = SERVER"; // LOCAL
+    private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = UTC"; // LOCAL
     private static final String driver = "com.mysql.cj.jdbc.Driver"; // Driver reference
     private static final String userName = "sqlUser"; // Username
     private static String password = "Passw0rd!"; // Password
